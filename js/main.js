adf574815a09212f9ac9223e40832401cd0dbf68
hossensumon071 <hossensumon071@gmail.com>
2020-08-23 05:30:35 +0600

    created timeline and progress bar

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ $(document).ready(function(){
  $('#profile__riple').ripples({
   resolution: 512,
   dropRadius: 10,
+ });
+
+
+ const bars = document.querySelectorAll('.progress__bar');
+
+ bars.forEach(function(bar){
+  let percentage = bar.dataset.percent;
+  let tooltip = bar.children[0];
+  tooltip.innerText = percentage + '%';
+  bar.style.width = percentage + '%';
  })
+
 
 })
