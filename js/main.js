25ea51bf18ae81c480064ec295f0f4ea286dc435
hossensumon071 <hossensumon071@gmail.com>
2020-08-23 06:53:19 +0600

    added javascript counter animation

--- js/main.js
+++ js/main.js
@@ -13,7 +13,53 @@ $(document).ready(function(){
   let tooltip = bar.children[0];
   tooltip.innerText = percentage + '%';
   bar.style.width = percentage + '%';
- })
+ });
 
+ // counter
+const counters = document.querySelectorAll('.counter');
+console.log(counters);
+function runCounter () {
+ counters.forEach(counter => {
+  counter.innerText = 0;
+  let target = +counter.dataset.count;
+  let step = target / 100;
+
+
+  let countIt = function() {
+   let displayedCount = +counter.innerText;
+   if(displayedCount < target) {
+    counter.innerText = Math.ceil( displayedCount + step);
+
+    setTimeout(countIt, 1)
+   } else {
+    counter.innerText = target;
+   }
+  }
+
+  countIt();
+ })
+};
+
+
+
+
+let counterSection = document.querySelector('.counter__section');
+
+
+let options = {
+ rootMargin : '0px 0px -200px 0px'
+}
+let done = 0;
+
+const sectionObserver = new IntersectionObserver(function(entries){
+
+
+ if(entries[0].isIntersecting && done !== 1) {
+  done = 1;
+  runCounter();
+ }
+},options)
+
+sectionObserver.observe(counterSection);
 
 })
